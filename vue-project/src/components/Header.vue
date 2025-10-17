959ffb0771861451c461e58cda2f6a8c5f0b3b08
Front-end-4 <31347903+waggneroliveira@users.noreply.github.com>
2025-10-17 10:19:05 -0300

    Add registration dropdown and modals to header
    Enhanced the Header component by replacing the simple 'Cadastre-se' button with a dropdown for user type selection (Acompanhante or Cliente), each opening a styled modal registration form. Updated main.css with new color variables, gradients, font utilities, and custom styles to support the new UI elements and improve overall appearance.

--- vue-project/src/components/Header.vue
+++ vue-project/src/components/Header.vue
@@ -25,11 +25,84 @@
 
                 <div class="row gap-3">
                     <!-- Menu Direita -->
-                    <div class="navbar-nav navbar-top w-100 justify-content-end">
-                        <button class="btn btn-outline-light me-2">
-                            <i class="bi bi-person-plus"></i> Cadastre-se
-                        </button>
-                        <button class="btn btn-transparent border me-2">
+                    <div class="navbar-nav navbar-top w-100 justify-content-end gap-4">
+                        <!-- Botão com Dropdown -->
+                        <div class="dropdown d-flex">
+                            <button class="btn btn-dropdown clean d-flex justify-content-center align-items-center gap-2 text-white" type="button" id="dropdownCadastro" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="bi bi-person-plus"></i> 
+                                <svg width="10" height="13" viewBox="0 0 10 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10 11.6667C10 11.9735 9.75127 12.2222 9.44444 12.2222C9.13762 12.2222 8.88889 11.9735 8.88889 11.6667C8.88889 9.51889 7.14777 7.77778 5 7.77778C2.85223 7.77778 1.11111 9.51889 1.11111 11.6667C1.11111 11.9735 0.86238 12.2222 0.555556 12.2222C0.248731 12.2222 0 11.9735 0 11.6667C0 8.90524 2.23858 6.66667 5 6.66667C7.76142 6.66667 10 8.90524 10 11.6667ZM5 6.11111C3.31246 6.11111 1.94444 4.74309 1.94444 3.05556C1.94444 1.36802 3.31246 0 5 0C6.68754 0 8.05556 1.36802 8.05556 3.05556C8.05556 4.74309 6.68754 6.11111 5 6.11111ZM5 5C6.07389 5 6.94444 4.12944 6.94444 3.05556C6.94444 1.98167 6.07389 1.11111 5 1.11111C3.92611 1.11111 3.05556 1.98167 3.05556 3.05556C3.05556 4.12944 3.92611 5 5 5Z" fill="white"/>
+                                </svg>
+
+                                Cadastre-se
+                            </button>
+                            <ul class="dropdown-menu border-with-gradient p-0 m-0" aria-labelledby="dropdownCadastro">
+                                <li><a class="dropdown-item d-flex justify-conten-center align-items-center gap-2 text-white dropdown-item-companion" href="#" data-bs-toggle="modal" data-bs-target="#modalAcompanhante"><span class="bg-white radius d-inline-block position-relative"></span> Acompanhante</a></li>
+                                <li><a class="dropdown-item d-flex justify-conten-center align-items-center gap-2 text-white dropdown-item-client" href="#" data-bs-toggle="modal" data-bs-target="#modalCliente"><span class="bg-white radius d-inline-block position-relative"></span> Cliente</a></li>
+                            </ul>
+                        </div>
+
+                        <!-- Modal Cadastro Acompanhante -->
+                        <div class="modal fade" id="modalAcompanhante" tabindex="-1" aria-labelledby="modalAcompanhanteLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content red-secundary-gradient text-light pb-4">
+                                    <div class="modal-header flex-column">
+                                        <button type="button" class="btn-close text-gold-gradient d-flex justify-content-center align-items-center font-40" data-bs-dismiss="modal" aria-label="Fechar">x</button>
+                                        <div class="row flex-column justify-content-center align-items-center">
+                                            <img loading="lazy" :src="logo" alt="Firula" class="logo-modal">
+                                            <h5 class="modal-title text-berge font-30 text-center" id="modalAcompanhanteLabel">Cadastro de Acompanhante</h5>
+                                            <p class="text-berge font-15 text-center">Entre para nossa plataforma e destaque-se!</p>
+                                        </div>
+                                    </div>
+                                    <div class="modal-body mt-3">
+                                        <form id="formAcompanhante" class="col-10 m-auto text-center">
+                                            <div class="mb-3">
+                                                <input type="text" class="form-control red-gradient radius-10" placeholder="Nome" name="nome" required>
+                                            </div>
+                                            <div class="mb-3">
+                                                <input type="email" class="form-control red-gradient radius-10" placeholder="E-mail" name="email" required>
+                                            </div>
+                                            <div class="mb-5">
+                                                <input type="password" class="form-control red-gradient radius-10" placeholder="Senha" name="senha" required>
+                                            </div>
+                                            <button type="submit" class="btn gold-gradient w-100 text-red-primary font-18 register">Cadastrar</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Modal Cadastro Cliente -->
+                        <div class="modal fade" id="modalCliente" tabindex="-1" aria-labelledby="modalClienteLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content red-secundary-gradient text-light pb-4">
+                                    <div class="modal-header flex-column">
+                                        <button type="button" class="btn-close text-gold-gradient d-flex justify-content-center align-items-center font-40" data-bs-dismiss="modal" aria-label="Fechar">x</button>
+                                        <div class="row flex-column justify-content-center align-items-center">
+                                            <img loading="lazy" :src="logo" alt="Firula" class="logo-modal">
+                                            <h5 class="modal-title text-berge font-30 text-center" id="modalAcompanhanteLabel">Cadastro do Cliente</h5>
+                                            <p class="text-berge font-15 text-center">Acesse acompanhantes exclusivos – cadastre-se!</p>
+                                        </div>
+                                    </div>
+                                    <div class="modal-body mt-3">
+                                        <form id="formCliente" class="col-10 m-auto text-center">
+                                            <div class="mb-3">
+                                                <input type="text" class="form-control red-gradient radius-10" placeholder="Nome" name="nome" required>
+                                            </div>
+                                            <div class="mb-3">
+                                                <input type="email" class="form-control red-gradient radius-10" placeholder="E-mail" name="email" required>
+                                            </div>
+                                            <div class="mb-5">
+                                                <input type="password" class="form-control red-gradient radius-10" placeholder="Senha" name="senha" required>
+                                            </div>
+                                            <button type="submit" class="btn gold-gradient w-100 text-red-primary font-18 register">Cadastrar</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <button class="btn text-berge border-with-gradient radius-10 px-4 announcement">
                             <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M12.2363 2.98352H11.7665V1.07712C11.7665 0.72226 11.5919 0.39039 11.2996 0.189268C11.0065 -0.0118728 10.6344 -0.0556757 10.3033 0.0721805L5.31096 1.99587H2.79679C1.26172 1.99587 0.0132262 3.24648 0.0132262 4.78367C-0.110327 6.10611 0.640334 7.39916 1.90529 7.80703V12.1013C1.97281 14.2534 5.04156 14.2605 5.10978 12.1013V11.2055C5.10188 10.7005 4.35123 10.7034 4.34404 11.2055V12.1013C4.34404 12.5782 3.96978 12.9668 3.50934 12.9668C3.04746 12.9668 2.67105 12.5789 2.67105 12.1013V7.95874C2.57336 7.97095 4.36557 7.96377 4.34404 7.96593V9.67411C4.35266 10.1769 5.10116 10.1784 5.10978 9.67411V7.96593H5.30302L10.2861 9.98506C10.9621 10.2832 11.7896 9.7279 11.7673 8.9873V7.08089H12.2371C14.8022 7.0421 14.8059 3.02314 12.2371 2.98287L12.2363 2.98352ZM11.0008 3.3671V8.98867C11.0072 9.20202 10.7695 9.36436 10.5734 9.27672L5.76422 7.32783V3.91223C5.75632 3.4094 5.00638 3.40868 4.99847 3.91223V7.2015C4.68672 7.18498 2.63013 7.24747 2.36717 7.15193C1.71205 7.01258 1.18984 6.5507 0.940574 5.95592H2.54532C3.05031 5.94658 3.04744 5.19808 2.54532 5.19018C2.54532 5.18299 0.766024 5.20383 0.778234 5.18299V4.78504C0.778234 4.78216 0.778953 4.77929 0.778953 4.77641H2.11432C2.32622 4.77641 2.49718 4.60545 2.49718 4.39355C2.49718 4.18165 2.32622 4.01069 2.11432 4.01069H0.933385C1.23723 3.27943 1.95772 2.76368 2.79602 2.76368H5.38062C5.42659 2.75435 5.47185 2.75004 5.51854 2.73854L8.90049 1.4355V4.18027C8.90767 4.6831 9.65905 4.68382 9.66623 4.18027V1.14036L10.5778 0.789088C10.7717 0.705763 11.0073 0.867385 11.0002 1.07857V3.36784L11.0008 3.3671ZM13.3913 5.16076C13.3913 5.79791 12.8734 6.31582 12.2363 6.31582H11.7665V3.74998H12.2363C13.0035 3.75285 13.4761 4.43238 13.3913 5.1615V5.16076Z" fill="#C0BDAD"/>
                             <path d="M16.4787 4.6499H15.6448C15.1412 4.65852 15.1412 5.40774 15.6448 5.41565H16.4787C16.9816 5.40774 16.9823 4.65781 16.4787 4.6499Z" fill="#C0BDAD"/>
@@ -40,9 +113,9 @@
 
                             <i class="bi bi-megaphone"></i> Quero anunciar!
                         </button>
-                        <button class="btn btn-outline-light">
+                        <!-- <button class="btn btn-outline-light">
                             <i class="bi bi-box-arrow-in-right"></i> Login
-                        </button>
+                        </button> -->
                     </div>
                     <!-- Menu -->
                     <div class="collapse navbar-collapse navbar-bottom w-100 justify-content-end" id="navbarNav">
